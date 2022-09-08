54f65d08c193eb6219836e51ba4e5b3e2753662b
jvscursulim <josevictor.s.scursulim@gmail.com>
2022-09-08 17:00:28 -0300

    Filtering negative numbers as displacement

--- crypto/caesar_cipher.py
+++ crypto/caesar_cipher.py
@@ -16,21 +16,27 @@ class CaesarCipher:
 
         Args:
             word (str): A word to be encrypted/decrypted.
-            displacement (int): An integer number that represents the displacement 
-                                that will be applied in the alphabet.
+            displacement (int): A positive integer number that represents
+                                the displacement that will be applied in the 
+                                alphabet.
             decrypt_mode (bool, optional): Puts the method on encryption mode. 
                                            Defaults to False.
 
         Returns:
             str: A encrypted/decrypted word.
         """
-        sign = (-1)**(int(decrypt_mode))
-        char_idx_array = np.array([self.ALPHABET.index(char) for char in word])
-        char_idx_array_after_displacement = (char_idx_array + sign*displacement)%len(self.ALPHABET)
-        
-        new_word = ""
-        
-        for index in char_idx_array_after_displacement:
-            new_word += self.ALPHABET[index]
+        if displacement >= 0:
             
-        return new_word
+            sign = (-1)**(int(decrypt_mode))
+            char_idx_array = np.array([self.ALPHABET.index(char) for char in word])
+            char_idx_array_after_displacement = (char_idx_array + sign*displacement)%len(self.ALPHABET)
+            
+            new_word = ""
+            
+            for index in char_idx_array_after_displacement:
+                new_word += self.ALPHABET[index]
+                
+            return new_word
+        else:
+            
+            raise ValueError("Negative number! This function only accepts positive integer numbers as displacement!")
